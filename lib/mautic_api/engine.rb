57ef0696595f0453c2761e1a164a669c4d680b94
trampos.co <marcel@trampos.co>
2016-11-21 15:56:01 -0200

    trying again

--- lib/mautic_api/engine.rb
+++ lib/mautic_api/engine.rb
@@ -5,7 +5,7 @@ module MauticApi
     isolate_namespace MauticApi
 
     initializer :mautic_api do
-      ActiveAdmin.application.load_paths.unshift Dir[File.dirname(__FILE__) + '/mautic_api/admin']
+      ActiveAdmin.application.load_paths.unshift Dir[File.dirname(__FILE__) + '/admin']
     end
   end
 end
