1d1e06496bfaa36437ce36d04d03808993c643ba
trampos.co <marcel@trampos.co>
2016-11-29 18:41:13 -0200

    Segments methods

--- lib/mautic_api/api/segments.rb
+++ lib/mautic_api/api/segments.rb
@@ -3,7 +3,29 @@ module MauticApi
   class Segments < Api
     
     ENDPOINT = 'segments'
+
+    # Add a contact to the segment
+    #
+    # @param int $id      Segment ID
+    # @param int $contactId Contact ID
+    #
+    # @return array|mixed
     
+    def addContact id, contact_id
+      return make_request("#{self.endpoint}/#{id}/contact/add/#{contact_id}", {}, :post)
+    end
+
+    # Remove a contact from the segment
+    #
+    # @param int $id      Segment ID
+    # @param int $contactId Contact ID
+    #
+    # @return array|mixed
+    
+    def removeContact id, contact_id
+      return make_request("#{self.endpoint}/#{id}/contact/remove/#{contact_id}", {}, :post)
+    end
+
   end
   
 end
