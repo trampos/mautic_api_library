4919ef5a6662a395630755de69114cc4024ad155
trampos.co <marcel@trampos.co>
2016-11-29 18:58:56 -0200

    Context Not Found

--- lib/mautic_api.rb
+++ lib/mautic_api.rb
@@ -11,6 +11,7 @@ module MauticApi
   autoload :Api,                            'mautic_api/api/api'
   autoload :Contacts,                       'mautic_api/api/contacts'
   autoload :Companies,                      'mautic_api/api/companies'
+  autoload :Segments,                       'mautic_api/api/segments'
   autoload :ContextNotFoundException,       'mautic_api/exception/context_not_found_exception'
 
   class Context
